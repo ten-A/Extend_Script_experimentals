3cc6b9386c1830bf94dadcbf936ac66ec81e4558
Ten <ten@chuwa.iobb.net>
2016-12-28 15:50:00 +0900

    Update XMPtool.jsx

--- XMPtool.jsx
+++ XMPtool.jsx
@@ -1,16 +1,16 @@
 XMPtool = {
 	ns : "http://ns.chuwa.sytes.net/idcomment/1.0/",
 	prefix : "ID_meta:",//custom metada
-  f : new Object(),
+  	f : new Object(),
 	read : function(prop){//read exist custom metadata.
-    if(xmpLib==undefined) var xmpLib = new ExternalObject('lib:AdobeXMPScript');
+   		if(xmpLib==undefined) var xmpLib = new ExternalObject('lib:AdobeXMPScript');
 		var xmpFile = new XMPFile(this.f.fsName, XMPConst.UNKNOWN, XMPConst.OPEN_FOR_READ);
 		var xmpPackets = xmpFile.getXMP();
 		var xmp = new XMPMeta(xmpPackets.serialize());
 		alert(xmp.getProperty(this.ns, prop).toString());
 		},
 	write : function(prop, val){ //argumetns{prop:String/property of custom metadata, val1:String/value} 
-    if(xmpLib==undefined) var xmpLib = new ExternalObject('lib:AdobeXMPScript');
+  		if(xmpLib==undefined) var xmpLib = new ExternalObject('lib:AdobeXMPScript');
 		var xmpFile = new XMPFile(this.f.fsName, XMPConst.UNKNOWN, XMPConst.OPEN_FOR_UPDATE);
 		var xmp = xmpFile.getXMP();
 		var mt = new XMPMeta(xmp.serialize());
