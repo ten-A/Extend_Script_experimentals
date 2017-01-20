27c0a25ef3b7f2e0a324355d461ee1c06ee982b6
Ten <ten@chuwa.iobb.net>
2017-01-20 15:57:35 +0900

    update...

--- HttpClient.jsx
+++ HttpClient.jsx
@@ -1,1 +1,11 @@
-﻿try {	var libPath = "/HttpClient.framework";	if (eo==null) var eo = new ExternalObject("lib:" + libPath);	var rt  = "";	rt = eo.getHTTP("http://chuwa.iobb.net");	alert(rt);	eo.unload();	} catch (e){		alert("error: " + e);		}
+try {
+	var libPath = "/HttpClient.framework";
+	if (eo==null) var eo = new ExternalObject("lib:" + libPath);
+	var rt  = "";
+	rt = eo.getHTTP("http://chuwa.iobb.net");
+	alert(rt);
+	eo.unload();
+	} catch (e){
+		alert("error: " + e);
+		}
+
