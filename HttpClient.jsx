7d92b507215c17af6474595ead582b90cc237367
Ten <ten@chuwa.iobb.net>
2017-01-20 15:55:52 +0900

    External Object Library

---
+++ HttpClient.jsx
@@ -0,0 +1,1 @@
+﻿try {	var libPath = "/HttpClient.framework";	if (eo==null) var eo = new ExternalObject("lib:" + libPath);	var rt  = "";	rt = eo.getHTTP("http://chuwa.iobb.net");	alert(rt);	eo.unload();	} catch (e){		alert("error: " + e);		}
