54fa8628d43ba1b2afec8643b6bf6331c17fa133
Ten <ten@chuwa.iobb.net>
2017-02-10 16:28:03 +0900

    Update rectToLine.jsx

--- rectToLine.jsx
+++ rectToLine.jsx
@@ -1,1 +1,35 @@
-﻿/*Adobe Illustrators Javascript.   Convert Rectangles to lines Sample code.*/(function (){	var tg = app.selection;	for (var i=0;i<tg.length;i++){		if (tg[i].typename!="PathItem") continue;		var ln = app.activeDocument.pathItems.add();		ln.filled = false;		ln.strokeColor = tg[i].fillColor;		if (tg[i].width>tg[i].height){			addPoint(ln, [tg[i].geometricBounds[0],					tg[i].geometricBounds[1] - tg[i].height / 2]);			addPoint(ln, [tg[i].geometricBounds[2],					tg[i].geometricBounds[1] - tg[i].height / 2]);			ln.strokeWidth = tg[i].height;			}		else{			addPoint(ln, [tg[i].geometricBounds[0] + tg[i].width / 2,					tg[i].geometricBounds[1]]);			addPoint(ln, [tg[i].geometricBounds[0] + tg[i].width / 2,					tg[i].geometricBounds[3]]);			ln.strokeWidth = tg[i].width;			}		tg[i].remove();		}	function addPoint(tgt, pos){		var pt = tgt.pathPoints.add();		pt.anchor = pos;		pt.leftDirection = pos;		pt.rightDirection = pos;		}	})();
+/*Adobe Illustrators Javascript.
+   Convert Rectangles to lines Sample code.
+*/
+
+(function (){
+	var tg = app.selection;
+	for (var i=0;i<tg.length;i++){
+		if (tg[i].typename!="PathItem") continue;
+		var ln = app.activeDocument.pathItems.add();
+		ln.filled = false;
+		ln.strokeColor = tg[i].fillColor;
+		if (tg[i].width>tg[i].height){
+			addPoint(ln, [tg[i].geometricBounds[0],
+					tg[i].geometricBounds[1] - tg[i].height / 2]);
+			addPoint(ln, [tg[i].geometricBounds[2],
+					tg[i].geometricBounds[1] - tg[i].height / 2]);
+			ln.strokeWidth = tg[i].height;
+			}
+		else{
+			addPoint(ln, [tg[i].geometricBounds[0] + tg[i].width / 2,
+					tg[i].geometricBounds[1]]);
+			addPoint(ln, [tg[i].geometricBounds[0] + tg[i].width / 2,
+					tg[i].geometricBounds[3]]);
+			ln.strokeWidth = tg[i].width;
+			}
+		tg[i].remove();
+		}
+	function addPoint(tgt, pos){
+		var pt = tgt.pathPoints.add();
+		pt.anchor = pos;
+		pt.leftDirection = pos;
+		pt.rightDirection = pos;
+		}
+	}
+)();
