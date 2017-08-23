f72d1b965a1d5a7a0bff75edba2be333deacfc82
Ten <ten@chuwa.iobb.net>
2017-08-23 10:46:14 +0900

    Update trasparentChecker.jsx

--- trasparentChecker.jsx
+++ trasparentChecker.jsx
@@ -1,1 +1,26 @@
-﻿#targetengine "session"var checkBackgroundEvt = app.addEventListener("afterSelectionChanged",	function(ev){		try {		var slct = app.selection[0];		if (slct.hasOwnProperty("graphics") && slct.graphics[0].imageTypeName.indexOf("PDF")>-1)			if (!slct.graphics[0].pdfAttributes.transparentBackground) {				sendNotify("Alert!","Not Transparent!!");				//slct.remove();				}			}		catch(e){}		}	).name = "pdftransparent";function sendNotify(title, discription){	try{		var ntfyr = 'display notification "' + discription + '" with title "' + title + '"';		app.doScript(ntfyr, ScriptLanguage.APPLESCRIPT_LANGUAGE);		}	catch (e){}	}//Stop listener//app.eventListeners.itemByName("pdftransparent").remove();
+#targetengine "session"
+
+var checkBackgroundEvt = app.addEventListener("afterSelectionChanged",
+	function(ev){
+		try {
+		var slct = app.selection[0];
+		if (slct.hasOwnProperty("graphics") && slct.graphics[0].imageTypeName.indexOf("PDF")>-1)
+			if (!slct.graphics[0].pdfAttributes.transparentBackground) {
+				sendNotify("Alert!","Not Transparent!!");
+				//slct.remove();
+				}
+			}
+		catch(e){}
+		}
+	).name = "pdftransparent";
+
+function sendNotify(title, discription){
+	try{
+		var ntfyr = 'display notification "' + discription + '" with title "' + title + '"';
+		app.doScript(ntfyr, ScriptLanguage.APPLESCRIPT_LANGUAGE);
+		}
+	catch (e){}
+	}
+
+//Stop listener
+//app.eventListeners.itemByName("pdftransparent").remove();
