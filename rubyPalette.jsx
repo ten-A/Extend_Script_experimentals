2b128a35dc2b38ca4b080775b653c7fc3df2cbd6
Ten <ten@chuwa.iobb.net>
2018-06-20 11:11:54 +0900

    Update rubyPalette.jsx

--- rubyPalette.jsx
+++ rubyPalette.jsx
@@ -1,49 +1,51 @@
-#targetengine session
+(function(){
+	var ps;
+	if (app.selection[0] instanceof Character //if selection in not correct, It will terminate.
+			||app.selection[0] instanceof Word
+			||app.selection[0] instanceof InsertionPoint
+			||app.selection[0] instanceof Text) {
+		if (app.selection[0].toString().indexOf(InsertionPoint)>-1) {
+			ps = app.selection[0].index;
+			}
+		else {
+			ps = 0;
+			}
+		var idleTask = app.idleTasks.add({name:'rubyPalette', sleep:250})
+				.addEventListener(IdleEvent.ON_IDLE, task1);
 
-var ps;
-if (app.selection[0] instanceof Character //if selection isn't correct, It will terminate.
-		||app.selection[0] instanceof Word) {
-	if (app.selection[0].toString().indexOf(InsertionPoint)>-1) {
-		ps = app.selection[0].index;
+		var w = new Window ('palette', 'モノルビ処理', undefined); //Make UI
+		var tx = w.add('edittext', undefined,'', {multiline:false});
+		tx.characters = 20;
+		w.submitBtn = w.add('button', undefined, '適　用', {name:'ok'});
+		w.closelBtn = w.add('button', undefined, 'とじる', {name:'close'});
+		w.closelBtn.onClick = function(){
+			app.idleTasks.everyItem().remove();
+			w.close();
+			}
+		w.submitBtn.onClick = function(){ //Apply ruby string.
+			app.selection[0].rubyString = tx.text;
+			app.selection[0].rubyFlag = true;
+			};
+		w.show();
 		}
-	else {
-		ps = 0;
-		}
-	var idleTask = app.idleTasks.add({name:'insertCharacter', sleep:250})
+	function task1(ev){ //Main Idle task
+		app.idleTasks.itemByName("rubyPalette").remove(); //Remove old one.
+		if (app.selection[0] instanceof Character //if selection lost, task will stop schaduling next one.
+				||app.selection[0] instanceof Word
+				||app.selection[0] instanceof InsertionPoint
+				||app.selection[0] instanceof Text) {
+			//Do something you want to do. In this case, check selection and deffer old state, 
+			//Get the selections contents and pass back to the Interfaces textbox.
+			if (app.selection[0].index!=ps){ 
+				tx.text = app.selection[0].rubyString;
+				ps = app.selection[0].index; //keep selections state that needs to next task.
+			}
+		 idleTask = app.idleTasks.add({name:'rubyPalette', sleep:250}) //register new task.
 			.addEventListener(IdleEvent.ON_IDLE, task1);
-
-	var w = new Window ('palette', 'モノルビ処理', undefined); //Make UI
-	var tx = w.add('edittext', undefined,'', {multiline:false});
-	tx.characters = 20;
-	w.submitBtn = w.add('button', undefined, '適　用', {name:'ok'});
-	w.closelBtn = w.add('button', undefined, 'とじる', {name:'close'});
-	w.closelBtn.onClick = function(){
-		app.idleTasks.everyItem().remove();
-		w.close();
+		 }
+		else {
+			w.close();
+			return;
+			}
 		}
-	w.submitBtn.onClick = function(){ //Apply ruby string.
-		app.selection[0].rubyString = tx.text;
-		app.selection[0].rubyFlag = true;
-		};
-	w.show();
-	}
-
-
-function task1(ev){ //Main Idle task
-	app.idleTasks.everyItem().remove(); //Remove old one.
-	if (app.selection[0] instanceof Character //if selection lost, task will stop schaduling next one.
-			||app.selection[0] instanceof Word) {
-		//Do something you want to do. In this case, check selection and deffer old state, 
-		//Get the selections contents and pass back to the Interfaces textbox.
-		if (app.selection[0].index!=ps){ 
-			tx.text = app.selection[0].rubyString;
-			ps = app.selection[0].index; //keep selections state that needs to next task.
-		}
-	 idleTask = app.idleTasks.add({name:'insertCharacter', sleep:250}) //register new task.
-		.addEventListener(IdleEvent.ON_IDLE, task1);
-	 }
-	else {
-		w.close();
-		return;
-		}
- }
+	}());
