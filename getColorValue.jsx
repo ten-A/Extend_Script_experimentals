366db72ea2ded315248c384e8af42cc7ed4bedf2
Ten <ten@chuwa.iobb.net>
2018-08-02 13:34:45 +0900

    update

--- getColorValue.jsx
+++ getColorValue.jsx
@@ -3,15 +3,13 @@ var tg = app.selection[0];
 var str="";
 var ch =[];
 if (tg.fillColor instanceof RGBColor){
-	str = "#";
 	ch =["red","green","blue"];
 	for (var i=0;i<3;i++){
 		if (eval("tg.fillColor"+ch[i])==0) str += "00";
-		else str += eval("Number(tg.fillColor."+ch[i]+").toString(16)");
+		else str += ch[i] + ":" + eval("tg.fillColor."+ch[i]) + "\n";
 		}
 	}
 else {
-	str = "";
 	ch =["cyan","magenta","yellow","black"];
 	for (var i=0;i<4;i++){
 		str += ch[i] + ":" + eval("tg.fillColor." + ch[i]) + "\n" ;
