878b6b1b879a922c5de69613713cc374033f3359
Ten <ten@chuwa.iobb.net>
2018-08-18 16:56:19 +0900

    for InDesign

---
+++ preFlightnotifyer.jsx
@@ -0,0 +1,20 @@
+#targetengine session
+
+var idleTask = app.idleTasks.add({name:'preftest', sleep:100})
+					.addEventListener(IdleEvent.ON_IDLE, task1);
+function task1(){
+	var flg = app.activeDocument.activeProcess.processResults;
+	if(flg.indexOf("No")<0) sendNotify ("Error!", flg);
+	app.idleTasks.itemByName("preftest").remove();
+	idleTask = app.idleTasks.add({name:'preftest', sleep:100})
+					.addEventListener(IdleEvent.ON_IDLE, task1);
+		return;
+		}
+
+function sendNotify(title,discription){
+	try{
+		var ntfyr = 'display notification "' + discription + '" with title "' + title + '"';
+		app.doScript(ntfyr , ScriptLanguage.APPLESCRIPT_LANGUAGE);
+		}
+	catch (e){}
+	}
