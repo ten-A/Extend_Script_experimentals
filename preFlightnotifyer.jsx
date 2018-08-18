7c09900029118de990028fbce252418640df8082
Ten <ten@chuwa.iobb.net>
2018-08-18 17:14:00 +0900

    update...

--- preFlightnotifyer.jsx
+++ preFlightnotifyer.jsx
@@ -2,9 +2,13 @@
 
 var idleTask = app.idleTasks.add({name:'preftest', sleep:100})
 					.addEventListener(IdleEvent.ON_IDLE, task1);
+var str = ""; 
 function task1(){
 	var flg = app.activeDocument.activeProcess.processResults;
-	if(flg.indexOf("No")<0) sendNotify ("Error!", flg);
+	if(flg.indexOf("No")<0&&str!=flg) {
+		sendNotify ("Error!", flg);
+		str = flg;
+		}
 	app.idleTasks.itemByName("preftest").remove();
 	idleTask = app.idleTasks.add({name:'preftest', sleep:100})
 					.addEventListener(IdleEvent.ON_IDLE, task1);
