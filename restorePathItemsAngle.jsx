626ae2696732423b4e965e76b0b18b9804f9de5b
Ten <agata@chuwa-print.co.jp>
2020-02-05 17:07:11 +0900

    Beautify...

--- restorePathItemsAngle.jsx
+++ restorePathItemsAngle.jsx
@@ -1,12 +1,12 @@
 (function(){
 	var tg = app.selection;
-	var pt = [], i = 0;
 	if(tg.length>2) return;
+	var pt = [], i = 0;
 	if(tg.length==2) {
 		for(var k=0;k<2;k++){
 			while (pt.length<2){
-			if(tg[k].pathPoints[i].selected==PathPointSelection.ANCHORPOINT) 
-				pt.push(tg[k].pathPoints[i].anchor);
+				if(tg[k].pathPoints[i].selected==PathPointSelection.ANCHORPOINT) 
+					pt.push(tg[k].pathPoints[i].anchor);
 				if(i==tg[k].pathPoints.length-1) break;
 				i++;
 				}
@@ -17,7 +17,7 @@
 	else {
 		while (pt.length<2){
 			if(tg[0].pathPoints[i].selected==PathPointSelection.ANCHORPOINT) 
-			pt.push(tg[0].pathPoints[i].anchor);
+				pt.push(tg[0].pathPoints[i].anchor);
 			if(i==tg[0].pathPoints.length-1) break;
 			i++;
 			}
@@ -27,5 +27,5 @@
 	var deg = Math.atan2(-pt[1][1]+pt[0][1], pt[1][0]-pt[0][0])/Math.PI*180;
 	if (tg[0].parent.typename=="CompoundPathItem") tg[0].parent.rotate(deg);
 	else tg[0].rotate(deg);
-}())
+	}())
 
